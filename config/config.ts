41bce3d0eb22614106ab00469db37c391e032e35
Goxiaoy <xy1996mail@gmail.com>
2023-02-10 13:55:08 +0800

    fix env

--- config/config.ts
+++ config/config.ts
@@ -4,7 +4,7 @@ import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 import routes from './routes';
 
-const { REACT_APP_ENV = 'dev' } = process.env;
+const { REACT_APP_ENV } = process.env;
 
 export default defineConfig({
   hash: true,
